bf2ca477a24199f75df0888389b8f7c42e878e64
ChrisMeeusen <cobolstinks@gmail.com>
2013-04-12 15:08:06 -0500

    Initial Commit

---
+++ Glimpse.ClaimsInspector/ClaimsInspector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Glimpse.AspNet.Extensions;
+using Glimpse.Core.Extensibility;
+using Microsoft.IdentityModel.Claims;
+
+namespace Glimpse.ClaimsInspector
+{
+    public class ClaimsInspector : TabBase
+    {
+        public override object GetData(ITabContext context)
+        {
+            var res = new List<string[]> { new[] { "Subject", "Type", "Value", "Value Type", "Issuer", "Original Issuer" } };
+            var httpContext = context.GetHttpContext();
+
+            var iPrincipal = (IClaimsPrincipal)httpContext.User;
+            var identity = (IClaimsIdentity)iPrincipal.Identity;
+
+            res.AddRange(identity.Claims.Select(c => new[] {  c.Subject==null?string.Empty:c.Subject.ToString(),c.ClaimType, 
+                c.Value, c.ValueType, c.Issuer ,c.OriginalIssuer }));
+
+            return res;
+        }
+
+        public override string Name
+        {
+            get { return "Claim Data"; }
+        }
+    }
+}
